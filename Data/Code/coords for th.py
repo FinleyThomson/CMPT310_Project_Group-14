8996aecc97f2d25c84c79bf0e17fec94845eeace
FinleyThomson <fgt@sfu.ca>
2026-07-29 21:07:09 -0700

    Refactor data loading and saving in coords for th.py

--- Data/Code/coords for th.py
+++ Data/Code/coords for th.py
@@ -1,7 +1,7 @@
 import numpy as np
 import pandas as pd
 
-df = pd.read_csv("C:/Users/finle/Documents/CMPT 310/Project/Data/MASTER/TH_DATA.csv")
+df = pd.read_csv("TH_DATA.csv")
 
 df["cluster_center_lon"] = df.groupby("Cluster_ID")["Longitude"].transform("mean")
 df["cluster_center_lat"] = df.groupby("Cluster_ID")["Latitude"].transform("mean")
